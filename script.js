638f65b35ef9bff00e68712618549cf496a77184
jaweriadev <jaweriaabdulkhaliq@gmail.com>
2024-01-30 15:02:55 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+// gsap.from(".header", {
+//   ease: "bounce",
+//   y: "-100%",
+//   duration: 1,
+// });
+// gsap.from(".link", {
+//   duration: 1,
+//   opacity: 0,
+//   delay: 1,
+//   stagger: 0.5,
+// });
+// gsap.from(".right", {
+//   duration: 2,
+//   x: "-100vw",
+//   ease: "power2.in",
+// });
+// gsap.from(".left", {
+//   duration: 1,
+//   x: "-100%",
+//   delay: 1.5,
+// });
+// gsap.to(".footer", {
+//   duration: 1,
+//   y: 0,
+//   ease: "elastic",
+//   delay: 2.5,
+// });
+// gsap.fromTo(
+//   ".button",
+//   {
+//     opacity: 0,
+//     scale: 0,
+//     rotation: 720,
+//   },
+//   { duration: 1, delay: 3.5, opacity: 1, scale: 1, rotation: 0 }
+// );
+const timeline = gsap.timeline({ defaults: { duration: 1 } });
+timeline
+
+  .from(".header", {
+    ease: "bounce",
+    y: "-100%",
+  })
+  .from(".link", {
+    opacity: 0,
+    delay: 1,
+    stagger: 0.5,
+  })
+  .from(
+    ".right",
+    {
+      x: "-100vw",
+      ease: "power2.in",
+    },
+    1
+  )
+  .from(
+    ".left",
+    {
+      x: "-100%",
+    },
+    "<.5"
+  )
+  .to(".footer", {
+    y: 0,
+    ease: "elastic",
+  })
+  .fromTo(
+    ".button",
+    {
+      opacity: 0,
+      scale: 0,
+      rotation: 720,
+    },
+    { opacity: 1, scale: 1, rotation: 0 }
+  );
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+  timeline.timeScale(3);
+  timeline.reverse();
+});
